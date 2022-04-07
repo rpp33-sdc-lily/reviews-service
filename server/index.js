8c2bd60145b2512aa1611d288ef001257b66a488
Rudy Sarmiento <83668987+rudyesar@users.noreply.github.com>
2022-04-06 17:19:19 -0700

    set up initial routes

--- server/index.js
+++ server/index.js
@@ -9,12 +9,36 @@ app.get('/test', (req, res) => {
   res.send({message: 'passing the test!'})
 });
 
+app.get('/reviews',(req, res) => {
+
+  res.status(200).send({});
+});
+
+app.get('/reviews/meta', (req, res) => {
+
+  res.status(200).send({});
+});
+
+app.post('/reviews', (req, res) => {
+
+  res.status(200).send({});
+});
+
+app.put('/reviews/:review_id/helpful', (req, res) => {
+
+  res.status(200).send({});
+});
+
+app.put('/reviews/:review_id/report', (req, res) => {
+
+  res.status(200).send({});
+});
 
 
 let port = 3000;
 
-// app.listen(port, function() {
-//   console.log(`listening on port ${port}`);
-// });
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
 
 module.exports = app;
