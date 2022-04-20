3515103002546ad193309b67c07597a9f6396805
Rudy Sarmiento <83668987+rudyesar@users.noreply.github.com>
2022-04-19 19:02:17 -0700

    modified code to make compatible with running api integration tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,17 +1,93 @@
-const app = require('../server/index.js');
-const supertest = require('supertest');
-const request = supertest(app);
 
-describe('GET/', () => {
-  let res;
+const request = require('supertest');
+const server = require('../server/index.js');
 
-  beforeAll(async() => {
-    res = await request.get('/test')
+describe('Test API', () => {
+
+  beforeAll(done => {
+    done();
   });
 
-  it('Gets the test endpoint', async () => {
-    expect(res.status).toBe(200)
-    expect(res.body.message).toBe('passing the test!')
-  })
+  test('easy generic test on /', async() => {
+    // await request(server).get(`/reviews/?page=1&count=2&sort='newest'&product_id=999997`)
+    await request(server).get(`/`)
+    .expect(200)
+    .then((response) => {
+      expect(response.text).toEqual('anything');
+    });
+  });
 
-})
+  test('Gets the test endpoint', (done) => {
+    request(server)
+      .get('/test')
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        res.body.message = 'passing the test!';
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  afterAll(done => {
+    server.close();
+    done();
+  });
+});
+
+
+//previous method
+// const supertest = require('supertest');
+
+// describe('Test API', () => {
+//   let app;
+
+//   beforeEach(async() => {
+//     app = require('../server/index.js');
+//   });
+
+//   test(('Gets the test endpoint'), async() => {
+
+//     await supertest(app).get('/test')
+//     .then(res => {
+//       expect(res.status).toBe(200);
+//       expect(res.body.message).toBe('passing the test!');
+//     })
+//   })
+// });
+
+
+
+// FIRST METHOD
+
+// const app = require('../server/index.js');
+// const supertest = require('supertest');
+// const request = supertest(app);
+
+// describe('GET/', () => {
+//   let res;
+
+//   beforeAll(async() => {
+//     res = await request.get('/test')
+//   });
+
+//   it('Gets the test endpoint', async () => {
+//     expect(res.status).toBe(200)
+//     expect(res.body.message).toBe('passing the test!')
+//   })
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
