ba57d8214dbb22b69b5d0e221626268d941d4eb7
Rudy Sarmiento <83668987+rudyesar@users.noreply.github.com>
2022-05-01 22:26:20 -0700

    implemented redis cacheing on get meta data route

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ redisClient.on('connect', function() {
 });
 
 
-const defaultExpiration = 99;
+const defaultExpiration = 3600;
 
 
 app.get('/loaderio-628bd481c3f7576c7dea956314bea40f.txt', (req, res) => {
@@ -51,7 +51,7 @@ app.get('/reviews/', async (req, res) => {
         "count": parseInt(req.query.count),
         "results": response
       };
-      console.log('this is respones back to clinet,',typeof responseBackToClient)
+      console.log('this is respones back to client,',typeof responseBackToClient)
       return responseBackToClient;
     })
     .then(response => {
@@ -59,13 +59,12 @@ app.get('/reviews/', async (req, res) => {
       return response;
     })
     .catch(error => {
-      res.status(500).send({ message: 'error in getting reviews'});
+      res.status(500).send({ message: 'error in getting reviews from my db'});
       })
   })
-
   .catch(error => {
-    console.log('error in 58', error)
-    res.status(500).send({ message: 'error in line 55'});
+    console.log('error in getting reviews from redis', error)
+    res.status(500).send({ message: 'error in getting reviews from redis'});
     })
 
   res.status(200).send(reviews);
@@ -73,41 +72,31 @@ app.get('/reviews/', async (req, res) => {
 });
 
 
-// app.get('/reviews/',(req, res) => {
-//   var id = parseInt(req.query.product_id);
-//   models.getReviews(id)
-//   .then(response => {
-//     var responseBackToClient = {
-//       "product": id.toString(),
-//       "page": parseInt(req.query.page),
-//       "count": parseInt(req.query.count),
-//       "results": response
-//     };
-//     res.status(200).send(responseBackToClient);
-//   })
-//   .catch(error => {
-//     res.status(500).send({ message: 'error in getting reviews'});
-//   })
-// });
-
-
-app.get('/reviews/meta', (req, res) => {
+app.get('/reviews/meta', async (req, res) => {
   var id = parseInt(req.query.product_id);
 
-  // console.log('should be product id', id);
-  models.getMetaData(id)
-  .then(response => {
-    // console.log('response in server', response);
-    return helperFunctions.sortMetaData(response, id);
-  })
-  .then(response => {
-    res.status(200).send(response);
+  var metaData = await getOrSetCache(`reviews/meta?productId=${id}`, () => {
+    console.log('NOT GETTING FROM REDIS')
+    return models.getMetaData(id)
+    .then(response => {
+      // console.log('response in server', response);
+      return helperFunctions.sortMetaData(response, id);
+    })
+    .then(response => {
+      return response;
+    })
+    .catch(error => {
+      // console.log('error in getting meta data', error);
+      res.status(500).send({ message: 'error in getting meta data from my db'});
+    })
   })
   .catch(error => {
-    // console.log('error in getting meta data', error);
-    res.status(500).send({ message: 'error in getting meta data'});
-  })
+    console.log('error in getting meta data from redis', error)
+    res.status(500).send({ message: 'error in getting meta data from redis'});
+    })
+  res.status(200).send(metaData);
 });
+
 
 app.post('/reviews', (req, res) => {
   console.log('body parameters when posting a review', req.body);
@@ -148,46 +137,18 @@ app.put('/reviews/:review_id/report', (req, res) => {
 
 
 async function getOrSetCache(key, cb) {
-  // await redisClient.connect();
-  console.log('am i wiii?')
 
   return new Promise (async(resolve, reject) => {
-    console.log('am i looo?', key)
-    // if (error) {
-    //   console.log('error in promise', error)
-    //   return reject(error)
-    // }
-    // resolve('asdfsdf');
-    console.log('redisClient', redisClient.get)
-    // return redisClient.get(key, (error, data) => {
-    //   console.log('am i here?')
-    //   if (error) {
-    //     console.log('inside if block')
-    //     // await redisClient.disconnect();
-    //     reject(error);
-    //   }
-    //   if (data != null) {
-    //     console.log('GETTING FROM REDIS')
-    //     // await redisClient.disconnect();
-    //     resolve(JSON.parse(data));
-    //   }
-    //   // const freshData = await cb();
-    //   // redisClient.setex(key, defaultExpiration, JSON.stringify(freshData));
-    //   // await redisClient.disconnect();
-    //   resolve('freshData');
-    // })(
+
     const value = await redisClient.get(key);
-    console.log('line 180000', value)
 
     if (value != null) {
-      console.log('value in if statement', value)
+      console.log('GETTING FROM REDIS')
       resolve(JSON.parse(value));
     } else {
-
-        // console.log('callback', cb())
         return cb()
         .then(freshData => {
-          console.log('!!!!!!1$#$freshData', freshData)
+          // console.log('!!!!!!1$#$freshData', freshData)
           redisClient.set(key, JSON.stringify(freshData));
           redisClient.expire(key, defaultExpiration);
           resolve(freshData);
@@ -196,35 +157,17 @@ async function getOrSetCache(key, cb) {
           console.log('error line 188', error)
           reject(error);
         })
-        // resolve('still doesnt resolve')
     }
-    resolve('this doesnt resolve anything')
-    // return redisClient.get(key)
-    // .then((response) => {
-    //   console.log('response in then', response);
-    //   if (response === null) {
-    //     const freshData = cb()
-    //     .then(response => {
-    //       redisClient.setex(key, defaultExpiration, JSON.stringify(freshData));
-    //       resolve(freshData);
-    //     })
-    //   }
-    //   resolve(response);
-    // })
-    // .catch(error => {
-    //   console.log('error for redisclient.get', error)
-    // })
-    //  resolve('asdfsdf');
+    // resolve('this doesnt resolve anything')
   })
   .then(response => {
-    console.log('response in then statement', response)
+    // console.log('response in then statement', response)
     return response;
   })
   .catch(error => {
-    console.log('error in line 169')
+    console.log('error in line 207', error)
     return;
   })
-  // console.log('error in line 171')
 
 }
 
@@ -251,32 +194,40 @@ module.exports = app;
 
 
 
-
-// .then(response => {
-//   console.log('successfully added char rating in DB');
-//   var photosArray = JSON.parse(reviewData.photos);
-//   console.log('photos array', photosArray)
-//   if(photosArray.length > 0) {
-//     photosArray.forEach(photo => {
-//       var addPhotoQuery = `INSERT INTO photos(photo_id, review_id, url)
-//                           VALUES(null, ${reviewID}, "${photo}");`;
-//       return db.queryAsync(addPhotoQuery)
-//       .then(response => {
-//         console.log('successfully added photo in DB');
-//         return;
-//       })
-//       .catch(error => {
-//         console.log('error in adding photo to DB', error);
-//       })
-//     })
-//   } else {
-//     console.log('RESPONSE', response);
-//     return;
-//   }
-// })
+// app.get('/reviews/',(req, res) => {
+//   var id = parseInt(req.query.product_id);
+//   models.getReviews(id)
+//   .then(response => {
+//     var responseBackToClient = {
+//       "product": id.toString(),
+//       "page": parseInt(req.query.page),
+//       "count": parseInt(req.query.count),
+//       "results": response
+//     };
+//     res.status(200).send(responseBackToClient);
+//   })
+//   .catch(error => {
+//     res.status(500).send({ message: 'error in getting reviews'});
+//   })
+// });
 
 
+// app.get('/reviews/meta', (req, res) => {
+//   var id = parseInt(req.query.product_id);
 
-
+//   // console.log('should be product id', id);
+//   models.getMetaData(id)
+//   .then(response => {
+//     // console.log('response in server', response);
+//     return helperFunctions.sortMetaData(response, id);
+//   })
+//   .then(response => {
+//     res.status(200).send(response);
+//   })
+//   .catch(error => {
+//     // console.log('error in getting meta data', error);
+//     res.status(500).send({ message: 'error in getting meta data'});
+//   })
+// });
 
 
